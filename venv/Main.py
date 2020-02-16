0b29cffdc1fd7cf32814d880cc52a0efd8c6d059
jjrryyaa <jjrryyaa@gmail.com>
2020-02-15 23:13:30 -0800

    changed it

--- venv/Main.py
+++ venv/Main.py
@@ -1,6 +1,35 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.request import Request
+from bs4 import BeautifulSoup
 
-html = urlopen('http://pythonscraping.com/pages/page1.html')
-print(html.read())
 
-test = 5
+def openURL(url):
+    try:
+        req = Request(url, headers = {"User-Agent": "Mozilla/5.0"})
+        html = urlopen(req)
+    except HTTPError as e:
+        print("HTTP error " + str(e.code))
+        return None
+    return html
+
+
+def getTitle(url):
+    html = openURL(url)
+    try:
+        bs = BeautifulSoup(html.read(), 'html.parser')
+        title = bs.body.h1
+    except AttributeError as e:
+        print("Attribute error")
+        return None
+    return title
+
+
+
+
+title = getTitle('http://www.pythonscraping.com/pages/page3.html')
+if title == None:
+    print('Title could not be found')
+else:
+    print(title)
+
